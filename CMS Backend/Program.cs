59153f3266b32fe9786f92fdd7b7ff7d1ce1e8ea
Fred-Michael <chinazorfredrick@gmail.com>
2020-09-09 23:06:05 +0100

    minor modifications to the program file

--- CMS Backend/Program.cs
+++ CMS Backend/Program.cs
@@ -17,7 +17,7 @@ namespace CMS_Backend
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CMS());
         }
     }
 }
